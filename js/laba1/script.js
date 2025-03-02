60a9a90066d02a28527b7e434309f86213dbea5e
LYarmoliuk <ludka.yarmolyuk@gmail.com>
2025-03-02 23:23:19 +0200

    js laba1 done

---
+++ js/laba1/script.js
@@ -0,0 +1,12 @@
+
+document.getElementById("messageContainer").textContent = "Hello world!";
+
+
+function showStudentName() {
+    
+    const studentName = "Liudmila";
+    console.log(studentName);
+}
+
+
+document.getElementById("nameButton").onclick = showStudentName;
